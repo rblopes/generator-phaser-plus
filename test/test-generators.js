2071056bace5196c6376db142a9ee2e60ea0ce46
Rafael Barbosa Lopes <rafael.barbosalopes@gmail.com>
2015-06-15 13:10:12 -0300

    Nest generators tests.

--- test/test-generators.js
+++ test/test-generators.js
@@ -5,11 +5,13 @@ var gulp = require('gulp');
 var defineTestTask = require('./helpers/defineTestTask');
 
 
-[ 'default', 'object', 'plugin', 'state' ]
-  .forEach(function (generator) {
-    require('./generators/' + generator + '-generator')(
-      gulp,
-      defineTestTask,
-      require('../slushfile.js/generators/' + generator + '/task')
-    );
-  });
+describe('generators', function () {
+  [ 'default', 'object', 'plugin', 'state' ]
+    .forEach(function (generator) {
+      require('./generators/' + generator + '-generator')(
+        gulp,
+        defineTestTask,
+        require('../slushfile.js/generators/' + generator + '/task')
+      );
+    });
+});
