a3bb00912128cd9ab96d7aa2d343e15051f53dbc
Rafael Barbosa Lopes <rafael.barbosalopes@gmail.com>
2015-06-15 12:49:27 -0300

    Tyding up helpers module.

--- slushfile.js/lib/helpers.js
+++ slushfile.js/lib/helpers.js
@@ -1,17 +1,13 @@
 'use strict';
 
 
-var _s      = require('lodash/string');
+var _       = require('lodash/string');
 var compose = require('lodash/function/compose');
 
 
-module.exports.trim     = _s.trim;
-module.exports.classify = compose(_s.trim, _s.capitalize, _s.camelCase);
-
-module.exports.json = function (input) {
-  return JSON.stringify(input);
-};
-
-module.exports.kebab = function (input) {
-  return _s.kebabCase(input);
+module.exports = {
+  trim    : _.trim,
+  json    : JSON.stringify,
+  kebab   : _.kebabCase,
+  classify: compose(_.capitalize, _.camelCase)
 };
