1e1ab27c5d321c4d3b3d008df2d58a43001dde89
Rafael Barbosa Lopes <rafael.barbosalopes@gmail.com>
2017-07-07 17:05:00 -0300

    Removed useless attributes in some generators.

--- generators/app/index.js
+++ generators/app/index.js
@@ -11,75 +11,60 @@ module.exports = class extends Generator {
     return prompt(this);
   }
 
-  get writing() {
-    return {
-      // Copy dotfiles.
-      dotfiles() {
-        this.fs.copy(
-          this.templatePath('dotfiles/editorconfig'),
-          this.destinationPath('.editorconfig'));
-        this.fs.copy(
-          this.templatePath('dotfiles/gitattributes'),
-          this.destinationPath('.gitattributes'));
-        this.fs.copy(
-          this.templatePath('dotfiles/gitignore'),
-          this.destinationPath('.gitignore'));
-      },
+  writing() {
+    //  Copy dotfiles.
+    this.fs.copy(
+      this.templatePath('dotfiles/editorconfig'),
+      this.destinationPath('.editorconfig'));
+    this.fs.copy(
+      this.templatePath('dotfiles/gitattributes'),
+      this.destinationPath('.gitattributes'));
+    this.fs.copy(
+      this.templatePath('dotfiles/gitignore'),
+      this.destinationPath('.gitignore'));
 
-      // Create project README.
-      readme() {
-        this.fs.copyTpl(
-          this.templatePath('README.md'),
-          this.destinationPath('README.md'),
-          this.variables);
-      },
+    //  Write project README.
+    this.fs.copyTpl(
+      this.templatePath('README.md'),
+      this.destinationPath('README.md'),
+      this.variables);
 
-      // Copy the project scripts and related assets.
-      app() {
-        this.fs.copy(
-          this.templatePath(`${this.baseTemplate}/**`),
-          this.destinationPath(),
-          {
-            globOptions: {
-              dot: true
-            }
-          }
-        );
-      },
+    //  Copy scripts and related files.
+    this.fs.copy(
+      this.templatePath(`${this.baseTemplate}/**`),
+      this.destinationPath(),
+      {
+        globOptions: {
+          dot: true
+        }
+      });
 
-      // Copy sample game assets.
-      assets() {
-        this.fs.copy(
-          [this.templatePath('static/**'), '!**/*.{html,json}'],
-          this.destinationPath('static/'));
-        this.fs.copyTpl(
-          this.templatePath('static/index.html'),
-          this.destinationPath('static/index.html'),
-          this.variables);
-      },
+    //  Copy sample game assets.
+    this.fs.copy(
+      [this.templatePath('static/**'), '!**/*.{html,json}'],
+      this.destinationPath('static/'));
+    this.fs.copyTpl(
+      this.templatePath('static/index.html'),
+      this.destinationPath('static/index.html'),
+      this.variables);
 
-      // Copy Gulp tasks.
-      tasks() {
-        this.fs.copyTpl(
-          this.templatePath('gulpfile.js/**'),
-          this.destinationPath('gulpfile.js/'),
-          this.variables);
-      },
+    //  Copy Gulp tasks.
+    this.fs.copyTpl(
+      this.templatePath('gulpfile.js/**'),
+      this.destinationPath('gulpfile.js/'),
+      this.variables);
 
-      // Set the generator configuration values.
-      yorc() {
-        this.config.defaults(Object.assign({
-          meta: {
-            createdWith: this.rootGeneratorVersion(),
-            creationDate: new Date().toISOString()
-          }
-        }, defaults[this.baseTemplate]));
+    //  Set default configuration values.
+    this.config.defaults(Object.assign({
+      meta: {
+        createdWith: this.rootGeneratorVersion(),
+        creationDate: new Date().toISOString()
       }
-    };
+    }, defaults[this.baseTemplate]));
   }
 
   install() {
-    // Prefer Yarn package manager, if available.
+    //  Prefer Yarn package manager, if available.
     if (detectInstalled.sync('yarn')) {
       this.yarnInstall();
     } else {
