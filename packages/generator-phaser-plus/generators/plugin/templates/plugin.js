fc56e65fcd28fcdec3fb1d54723a5ecd48596c0c
Rafael Barbosa Lopes <rafael.barbosalopes@gmail.com>
2018-02-11 16:22:36 -0200

    Added JSDoc in generated plugins.

--- packages/generator-phaser-plus/generators/plugin/templates/plugin.js
+++ packages/generator-phaser-plus/generators/plugin/templates/plugin.js
@@ -1,16 +1,22 @@
-/*
- * <%- name %>
- * <%- '='.repeat(name.length) %>
- *
- * My custom plugin.
- */
-
 export default class <%- name %> {
+  /**
+   * Register this plugin in Phaser plugin manager, so it can be added to
+   * scenes.
+   *
+   * @static
+   * @param {Phaser.Scene.Systems} sys - A reference to the scene system.
+   */
   static register(sys) {
     sys.plugins.constructor.register('<%- name %>', this, '<%- id %>');
     sys.plugins.installGlobal(sys, ['<%- name %>']);
   }
 
+  /**
+   * My custom plugin.
+   *
+   * @constructor
+   * @param {Phaser.Scene} scene - The game scene that owns this plugin.
+   */
   constructor(scene) {
     Object.defineProperty(this, 'scene', {value: scene, enumerable: true});
     Object.defineProperty(this, 'sys', {value: scene.sys, enumerable: true});
@@ -20,6 +26,9 @@ export default class <%- name %> {
     }
   }
 
+  /**
+   * Called when this plugin is booted by Phaser plugin manager.
+   */
   boot() {
     //  Bind this plugin to the life-cycle events of the scene.
     this.sys.events
@@ -32,36 +41,80 @@ export default class <%- name %> {
       .on('sleep', this.sleep, this)
       .on('wake', this.wake, this)
       .on('shutdown', this.shutdown, this)
-      .on('destroy', this.destroy, this)
+      .on('destroy', this.destroy, this);
   }
 
+  /**
+   * Called when a scene is started by the scene manager. The scene is now
+   * active, visible and running.
+   */
   start() {
   }
 
-  preUpdate(/* t, dt */) {
+  /**
+   * Called every time a scene in being updated - pre-update step.
+   *
+   * @param {number} t - The current internal clock value.
+   * @param {number} dt - The interval since last update.
+   */
+  preUpdate(t, dt) {
   }
 
-  update(/* t, dt */) {
+  /**
+   * Called every time a scene in being updated - update step.
+   *
+   * @param {number} t - The current internal clock value.
+   * @param {number} dt - The interval since last update.
+   */
+  update(t, dt) {
   }
 
-  postUpdate(/* t, dt */) {
+  /**
+   * Called every time a scene in being updated - post-update step.
+   *
+   * @param {number} t - The current internal clock value.
+   * @param {number} dt - The interval since last update.
+   */
+  postUpdate(t, dt) {
   }
 
+  /**
+   * Called when a scene is paused. A paused scene doesn't have its step run,
+   * but still renders.
+   */
   pause() {
   }
 
+  /**
+   * Called when a scene is resumed from a paused state.
+   */
   resume() {
   }
 
+  /**
+   * Called when a scene is put to sleep. A sleeping scene doesn't update or
+   * render, but isn't destroyed or shutdown. `preUpdate` events still fire.
+   */
   sleep() {
   }
 
+  /**
+   * Called when a scene is woken from a sleeping state.
+   */
   wake() {
   }
 
+  /**
+   * Called when a scene shuts down, it may then come back again later (which
+   * will invoke the `start` event) but should be considered dormant.
+   */
   shutdown() {
   }
 
+  /**
+   * Called when a scene is destroyed by the scene manager. All allocated
+   * resources should be cleaned up.
+   */
   destroy() {
     this.shutdown();
 
