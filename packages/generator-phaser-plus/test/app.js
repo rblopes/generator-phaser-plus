37264f6898fbcb80cc7c36f1fdb8695c7646842a
Rafael Barbosa Lopes <rafael.barbosalopes@gmail.com>
2018-03-14 20:40:14 -0300

    Removed redundant test assertions.
    Already covered by the `default` template package.

--- packages/generator-phaser-plus/test/app.js
+++ packages/generator-phaser-plus/test/app.js
@@ -23,73 +23,16 @@ describe(chalk.bold.cyan('generator-phaser-plus:app'), function () {
   it('creates the project', () =>
     runGenerator()
       .withPrompts({title, description})
-      .then(checkReadme)
-      .then(checkTasks)
-      .then(checkConfig)
-      .then(checkAssets)
-      .then(checkScripts));
+      .then(checkFiles));
 
-  function checkReadme() {
+  function checkFiles() {
     assert.fileContent([
       ['README.md', `# [${title}]`],
       ['README.md', `>   ${description}`]
     ]);
-  }
-
-  function checkTasks() {
-    assert.file([
-      'gulpfile.js/config/babel.js',
-      'gulpfile.js/config/webpack',
-      'gulpfile.js/config/webpack/uglify.js',
-      'gulpfile.js/config/webpack/plugins.js',
-      'gulpfile.js/config/webpack/index.js',
-      'gulpfile.js/config/paths.js',
-      'gulpfile.js/config/browsersync.js',
-      'gulpfile.js/index.js',
-      'gulpfile.js/tasks/copy-assets.js',
-      'gulpfile.js/tasks/serve.js',
-      'gulpfile.js/tasks/clean.js',
-      'gulpfile.js/tasks/dist.js',
-      'gulpfile.js/tasks/compile.js',
-      'gulpfile.js/lib/server.js',
-      'gulpfile.js/lib/webpack-middlewares.js',
-      'gulpfile.js/lib/webpack.js'
+    assert.fileContent([
+      ['package.json', `"title": ${JSON.stringify(title)}`],
+      ['package.json', `"description": ${JSON.stringify(description)}`]
     ]);
-  }
-
-  function checkConfig() {
-    assert.file([
-      '.editorconfig',
-      '.eslintrc.js',
-      '.gitattributes',
-      '.gitignore',
-      '.yo-rc.json',
-      'package.json'
-    ]);
-  }
-
-  function checkAssets() {
-    assert.file([
-      'app/index.html',
-      'app/static/favicon.ico',
-      'app/static/assets/phaser.png',
-      'app/static/assets/progress-bar.png',
-      'app/static/assets/splash-screen.png'
-    ]);
-  }
-
-  function checkScripts() {
-    assert.file([
-      'app/scripts/assets.js',
-      'app/scripts/index.js',
-      'app/scripts/objects/logo.js',
-      'app/scripts/scenes/game.js',
-      'app/scripts/scenes/index.js',
-      'app/scripts/scenes/splash-screen.js'
-    ]);
-    assert.fileContent(
-      'app/scripts/config.js',
-      `export const title = '${title}';`
-    );
   }
 });
