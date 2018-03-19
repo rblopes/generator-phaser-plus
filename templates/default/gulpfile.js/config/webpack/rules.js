e20571bb1d5eefa0c41e5db33936666243a91aba
Rafael Barbosa Lopes <rafael.barbosalopes@gmail.com>
2018-03-19 19:13:05 -0300

    Revised Webpack configuration and comments.

--- templates/default/gulpfile.js/config/webpack/rules.js
+++ templates/default/gulpfile.js/config/webpack/rules.js
@@ -2,66 +2,64 @@
  *  Webpack Rules
  *  =============
  *
- *  Configures how source code is processed by Webpack.
+ *  How sources are processed by Webpack.
  */
 
 const {dirs} = require('../paths');
 
 module.exports = [
-  //  -- WebGL Fragment and Vertex Shaders (GLSL) sources.
+  //  WebGL Fragment and Vertex Shaders (GLSL)
+  //  ----------------------------------------
+  //
+  //  Required by Phaser. Compile fragment and vertex shaders from Phaser.
   {
     test: /\.(frag|vert)$/,
-    use: [{
-      //  `raw-loader`
-      //  ------------
-      //
-      //  Required by Phaser. Used to compile fragment and vertex shaders
-      //  sources contained in the Phaser library.
-      loader: 'raw-loader'
-    }]
+    loader: 'raw-loader'
   },
 
-  //  -- JavaScript sources.
+  //  JavaScript Application Modules
+  //  ------------------------------
+  //
+  //  Compile application modules with Babel 7. Uses `@babel/preset-env` to
+  //  compatibility with current browsers and devices.
+  //
+  //  Reference:
+  //  - <https://github.com/babel/babel-loader#readme>
   {
     test: /\.js$/,
     include: dirs.scripts,
-    use: [{
-      //  `babel-loader`
-      //  --------------
-      //
-      //  Uses `@babel/preset-env` to compile modules compatible with current
-      //  browsers and devices.
-      //
-      //  Reference:
-      //    <https://github.com/babel/babel-loader#readme>
-      loader: 'babel-loader',
-      options: {
-        presets: [
-          [
-            '@babel/preset-env',
-            {
-              targets: {
-                browsers: 'last 2 versions'
-              },
-              useBuiltIns: 'usage'
-            }
-          ]
+    loader: 'babel-loader',
+    options: {
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            targets: {
+              browsers: 'last 2 versions'
+            },
+            useBuiltIns: 'usage'
+          }
         ]
-      }
-    }, {
-      //  `eslint-loader`
-      //  ---------------
-      //
-      //  Runs ESLint over application sources to detect lint issues. Issues
-      //  are reported in the terminal.
-      //
-      //  Reference:
-      //    <https://github.com/webpack-contrib/eslint-loader#readme>
-      loader: 'eslint-loader',
-      options: {
-        emitError: true,
-        emitWarning: true
-      }
-    }]
+      ]
+    }
+  },
+
+  //  ESLint
+  //  ------
+  //
+  //  Run ESLint over application modules to detect issues. Issues are
+  //  reported in the terminal.
+  //
+  //  Reference:
+  //  - <https://github.com/webpack-contrib/eslint-loader#readme>
+  {
+    test: /\.js$/,
+    include: dirs.scripts,
+    enforce: 'pre',
+    loader: 'eslint-loader',
+    options: {
+      emitError: true,
+      emitWarning: true
+    }
   }
 ];
